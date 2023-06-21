17907ac900aae846ba73e0dbf63e230ee0e52cb6
ouhoy <kimkinon@gmail.com>
2023-06-21 21:15:06 +0100

    Build react app using useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [advice, setAdvice] = useState("")
+    const [counter, setCounter] = useState(0)
+
+    async function getAdvice() {
+
+        const result = await fetch("https://api.adviceslip.com/advice");
+        const data = await result.json();
+        setAdvice(data.slip.advice)
+        setCounter(counter + 1)
+        return data
+    }
+
+    useEffect(() => {
+        getAdvice()
+    }, [])
+
+    return (
+        <div className="App">
+            <h4>{advice}</h4>
+            <button onClick={getAdvice}>Get Advice</button>
+            <Message count={counter} />
+
+        </div>
+    );
+}
+
+function Message(props) {
+    return (
+        <p>You have seen {props.count} advices</p>
+    )
 }
 
 export default App;
