67b4454ed920f5b31d80d441103bb8ceae85ca57
ouhoy <kimkinon@gmail.com>
2023-07-05 21:29:12 +0100

    Manage and Show Number of Steps
    Here each step is highlighted when it is reacted

--- initial_steps/src/App.js
+++ initial_steps/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import messages from "./data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    let step = 1;
+
+    return (
+        <div className="steps">
+
+            <div className="numbers">
+                <div className={step >= 1 ? "active" : ""}>1</div>
+                <div className={step >= 2 ? "active" : ""}>2</div>
+                <div className={step === 3 ? "active" : ""}>3</div>
+            </div>
+
+            <div className="message">Step {step}: {messages[step - 1]}</div>
+            <div className="buttons">
+                <button style={{backgroundColor: "#7f5bf3", color: "white"}}>Previous</button>
+                <button style={{backgroundColor: "#7f5bf3", color: "white"}}>Next</button>
+            </div>
+
+        </div>
+    )
+
 }
 
-export default App;
