aad6b33ae2d887975c08d2d31f6e3c11d4c6994e
ouhoy <kimkinon@gmail.com>
2023-07-05 23:00:06 +0100

    Close and re-render steps component

--- initial_steps/src/App.js
+++ initial_steps/src/App.js
@@ -3,6 +3,7 @@ import {useState} from "react";
 
 export default function App() {
     const [steps, setSteps] = useState(1)
+    const [isOpen, setIsOpen] = useState(true)
 
     function handlePrevious() {
         if (steps === 1) return;
@@ -15,24 +16,30 @@ export default function App() {
     }
 
     return (
-        <div className="steps">
+        <>
+            <button className="close" onClick={() => {
+                setIsOpen(!isOpen)
+            }}>&times;</button>
+            {isOpen && (<div className="steps">
 
-            <div className="numbers">
-                <div className={steps >= 1 ? "active" : ""}>1</div>
-                <div className={steps >= 2 ? "active" : ""}>2</div>
-                <div className={steps === 3 ? "active" : ""}>3</div>
-            </div>
+                <div className="numbers">
+                    <div className={steps >= 1 ? "active" : ""}>1</div>
+                    <div className={steps >= 2 ? "active" : ""}>2</div>
+                    <div className={steps === 3 ? "active" : ""}>3</div>
+                </div>
 
-            <div className="message">Step {steps}: {messages[steps - 1]}</div>
-            <div className="buttons">
-                <button style={{backgroundColor: "#7f5bf3", color: "white"}} onClick={handlePrevious}>Previous
-                </button>
-                <button style={{backgroundColor: "#7f5bf3", color: "white"}} onClick={handleNext}>Next
-                </button>
-            </div>
+                <div className="message">Step {steps}: {messages[steps - 1]}</div>
+                <div className="buttons">
+                    <button style={{backgroundColor: "#7f5bf3", color: "white"}} onClick={handlePrevious}>Previous
+                    </button>
+                    <button style={{backgroundColor: "#7f5bf3", color: "white"}} onClick={handleNext}>Next
+                    </button>
+                </div>
 
-        </div>
+            </div>)}
+        </>
     )
+
 
 }
 
