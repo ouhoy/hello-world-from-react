53200246cb11d6c3902101419116a719bb97a769
ouhoy <kimkinon@gmail.com>
2023-07-05 22:05:01 +0100

    Changing the state steps in the UI

--- initial_steps/src/App.js
+++ initial_steps/src/App.js
@@ -1,22 +1,34 @@
 import messages from "./data";
+import {useState} from "react";
 
 export default function App() {
+    const [steps, setSteps] = useState(1)
 
-    let step = 1;
+    function handlePrevious() {
+        if (steps === 1) return;
+        setSteps(steps - 1)
+    }
+
+    function handleNext() {
+        if (steps >= messages.length) return
+        setSteps(steps + 1)
+    }
 
     return (
         <div className="steps">
 
             <div className="numbers">
-                <div className={step >= 1 ? "active" : ""}>1</div>
-                <div className={step >= 2 ? "active" : ""}>2</div>
-                <div className={step === 3 ? "active" : ""}>3</div>
+                <div className={steps >= 1 ? "active" : ""}>1</div>
+                <div className={steps >= 2 ? "active" : ""}>2</div>
+                <div className={steps === 3 ? "active" : ""}>3</div>
             </div>
 
-            <div className="message">Step {step}: {messages[step - 1]}</div>
+            <div className="message">Step {steps}: {messages[steps - 1]}</div>
             <div className="buttons">
-                <button style={{backgroundColor: "#7f5bf3", color: "white"}}>Previous</button>
-                <button style={{backgroundColor: "#7f5bf3", color: "white"}}>Next</button>
+                <button style={{backgroundColor: "#7f5bf3", color: "white"}} onClick={handlePrevious}>Previous
+                </button>
+                <button style={{backgroundColor: "#7f5bf3", color: "white"}} onClick={handleNext}>Next
+                </button>
             </div>
 
         </div>
