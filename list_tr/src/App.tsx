622d29ed7be68b6e9f36c9d6e9d2f49696fbebe6
ouhoy <kimkinon@gmail.com>
2023-07-08 17:27:21 +0100

    Set up List App Skeleton

--- list_tr/src/App.tsx
+++ list_tr/src/App.tsx
@@ -2,11 +2,40 @@ import React from 'react';
 
 
 function App() {
-  return (
-    <div className="App">
-     Hello World
+    return (
+        <div className={"app"}>
+            <Logo/>
+            <From/>
+            <PackingList/>
+            <Stats/>
+
+        </div>
+    );
+}
+
+function Logo() {
+    return <h1>Random Practice List</h1>
+}
+
+function From() {
+    return <div className={"add-form"}>
+
+        <h3>Need something?</h3>
+
     </div>
-  );
+}
+
+function PackingList() {
+    return <div className={"list"}>
+        LIST
+    </div>
+}
+
+function Stats() {
+    return <footer className={"stats"}>
+
+        <em>You have X in your list and already packed X items</em>
+    </footer>
 }
 
 export default App;
