554b681dd000756a3b710bdc42651efb63039b8c
ouhoy <kimkinon@gmail.com>
2023-07-13 23:28:24 +0100

    set range input

--- more_react_state/src/app.js
+++ more_react_state/src/app.js
@@ -18,7 +18,8 @@ function Counter() {
 
     return (<div style={{marginTop: "5%"}}>
 
-        <center>
+        <center style={{display: "flex", flexDirection: "column", alignItems: "center",}}>
+            <input type={"range"} min={0} max={10} value={doubler} onChange={e=>setDoubler(+e.target.value)}/>
             <button onClick={() => setCounter(num => num - doubler)}>-</button>
 
             <span style={{margin: "0 16px"}}>Count: {count}</span>
