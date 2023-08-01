a99d81c6ef2976041937346d28e4294d45ec241a
ouhoy <kimkinon@gmail.com>
2023-08-01 23:45:56 +0100

    Apply Styes and Render List

--- eat-n-split/src/App.tsx
+++ eat-n-split/src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-
+import {initialFriends} from "./data";
 
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
+            <div className={"sidebar"}>
+                <FriendsList/>
+            </div>
+        </div>
+    );
+}
 
-    </div>
-  );
+function FriendsList() {
+    return <ul>
+        {initialFriends.map(friend => friend.name)}
+    </ul>
 }
 
 export default App;
